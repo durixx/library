bef81b29be3e51402da0ab36729047b69b1e36ff
serox2255 <marian22durco@gmail.com>
2022-11-08 14:30:27 +0100

    test commit

--- src/main/java/sk/durixx/library/LibraryApplication.java
+++ src/main/java/sk/durixx/library/LibraryApplication.java
@@ -10,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LibraryApplication {
 
     public static void main(String[] args) {
+        // run the application
         SpringApplication.run(LibraryApplication.class, args);
     }
 }
